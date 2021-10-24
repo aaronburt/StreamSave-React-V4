e29909b013e2b76495d458d4632ef9f1944ac8c4
aaronburt <email@aaronburt.co.uk>
2021-10-24 10:47:44 +0100

    naming error to do with GIT

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
-import Video from './video';
-import Home from './home';
+import Video from './Video';
+import Home from './Home';
 
 ReactDOM.render(
   <React.StrictMode>
