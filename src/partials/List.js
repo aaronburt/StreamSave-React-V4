cc1491149145384c8256a34ccefb8e2d8a28719f
aaronburt <email@aaronburt.co.uk>
2021-10-24 07:34:39 +0100

    Css and Renaming variables

--- src/partials/List.js
+++ src/partials/List.js
@@ -30,22 +30,22 @@ export default class List extends React.Component {
             let rsp = [];
 
             for(let [key, value] of Object.entries(this.state.payload)){                
-                let BGimg = pull_zone_url + value.guid + '/' + value.thumbnailFileName;
-                let Preimg = pull_zone_url + value.guid + '/preview.webp'
+                let BackgroundImage = pull_zone_url + value.guid + '/' + value.thumbnailFileName;
+                let PreviewImage = pull_zone_url + value.guid + '/preview.webp'
                 if(value.availableResolutions !== null){
                     rsp.push(
                 
-                        <div key={BGimg}>
+                        <div key={BackgroundImage} title={value.title}>
                             <a href={'/v/' + value.guid} value="es">
-                                <div preview={Preimg} url={BGimg} className="list-img" style={{ backgroundImage: `url(${BGimg})`}} onMouseLeave={this.mouseLeave} onMouseOver={this.mouseOver}></div>
-                                <div className="list-img-title">{value.title}</div>
+                                <div preview={PreviewImage} url={BackgroundImage} className="item-item" style={{ backgroundImage: `url(${BackgroundImage})`}} onMouseLeave={this.mouseLeave} onMouseOver={this.mouseOver}></div>
+                                <div className="item-img-title">{value.title}</div>
                             </a>
                         </div>
                     );
                 }
             }
 
-            return(<div className="list-container">{rsp}</div>);
+            return(<div className="item-container">{rsp}</div>);
         } else {
             return (<div></div>)
         }
